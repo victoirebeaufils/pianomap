c2b534358bc03adf541ca1ce9ce4d2b69b95d4a2
Victoire Beaufils <victoire.beaufil@gmail.com>
2020-03-26 10:27:22 -0500

    Added port env

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use((error, req, res, next) => {
 });
 mongoose
 .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-vvfpo.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`)
-.then(()=>{app.listen(5000)})
+.then(()=>{app.listen(process.env.PORT || 5000)})
 .catch( err =>{
   console.log(err);
 }
